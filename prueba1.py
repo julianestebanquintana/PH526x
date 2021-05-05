90cbb8715749b54b5ec449da4800a2c8331d46d6
Julian Quintana <julianestebanquintana@gmail.com>
2021-05-05 18:26:59 -0500

    First commit Starting on Python for research MOOC, by Harvard

---
+++ prueba1.py
@@ -0,0 +1,54 @@
+# n = 7
+# is_prime = True
+# for i in range(2,n):
+#     if n%i == 0:
+#         is_prime = False
+# print(is_prime)
+
+
+# n=100
+# number_of_times = 0
+# while n >= 1:
+#     n //= 2
+#     number_of_times += 1
+#     # print(n)
+#     # print(number_of_times)
+# print(number_of_times)
+
+# F = open("input.txt", "w")
+# F.write("Hello\nWorld")
+# F.close()
+# lines = []
+# for line in open("input.txt"):
+#     lines.append(line.strip())
+# print(lines)
+
+# def modify(mylist):
+#     mylist[0] *= 10
+#     return(mylist)
+# L = [1, 3, 5, 7, 9]
+# M = modify(L)
+# M is L
+
+# def is_vowel(letter):
+#     if letter in "aeiouy":
+#         return(True)
+#     else:
+#         return(False)
+
+
+# is_vowel(4)
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         N = 1
+#         for i in range(1, n+1):
+#             N *= i
+#         return(N)
+
+
+# print(factorial(4))
+
+
