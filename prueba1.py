7128f2d7c226296b7bc2f74a5c18acfb84e3d301
Julian Quintana <julianestebanquintana@gmail.com>
2021-05-09 15:33:29 -0500

    Adding HW 2, not yet solved

--- prueba1.py
+++ prueba1.py
@@ -51,4 +51,26 @@
 
 # print(factorial(4))
 
+# def increment(n):
+#     n += 1
+#     print(n)
 
+
+# n = 1
+# increment(n)
+# print(n)
+
+class NewList(list):
+    def remove_max(self):
+        self.remove(max(self))
+
+    def append_sum(self):
+        self.append(sum(self))
+
+
+x = NewList([1, 2, 3])
+while max(x) < 10:
+    x.remove_max()
+    x.append_sum()
+
+print(x)
